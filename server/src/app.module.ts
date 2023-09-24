5051fb842faf8277283ad7eb9bbd9a13844b2a9c
Igor <qwertyhock@gmail.com>
2023-09-24 22:06:59 +0300

    create users model database

---
+++ server/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { SequalizeConfigService } from './config/sequelizeConfig.service';
+import { databaseConfig } from './config/configuration';
+
+@Module({
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: SequalizeConfigService,
+    }),
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
