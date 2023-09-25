78439848e0a6deb7fbe91dabfcd745eb27cc2112
Igor <qwertyhock@gmail.com>
2023-09-25 18:11:46 +0300

    auth

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LocalAuthGuard } from 'src/auth/local.auth.guard';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post('/signup')
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Content-type', 'application/json')
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Post('/login')
+  @UseGuards(LocalAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Header('Content-type', 'application/json')
+  login(@Request() req) {
+    return { user: req.user, msg: 'Logged in' };
+  }
+}
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import * as bcrypt from 'bcrypt';
+import { User } from './users.model';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User)
+    private useModel: typeof User,
+  ) {}
+
+  findOne(filter: {
+    where: { id?: string; username?: string; email?: string };
+  }): Promise<User> {
+    return this.useModel.findOne({ ...filter });
+  }
+
+  async create(
+    CreateUserDto: CreateUserDto,
+  ): Promise<User | { warningMessage: string }> {
+    const user = new User();
+    const existingByUserName = await this.findOne({
+      where: { username: CreateUserDto.username },
+    });
+    const existingByUserEmail = await this.findOne({
+      where: { email: CreateUserDto.email },
+    });
+
+    if (existingByUserName) {
+      return { warningMessage: 'Пользователь с таким именем уже существует' };
+    }
+
+    if (existingByUserEmail) {
+      return { warningMessage: 'Пользователь с таким email уже существует' };
+    }
+
+    const hashedPassword = await bcrypt.hash(CreateUserDto.password, 10);
+    user.username = CreateUserDto.username;
+    user.password = hashedPassword;
+    user.email = CreateUserDto.email;
+
+    return user.save();
+  }
+}
